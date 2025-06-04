2810587377be7efd00aa147c155abb13c6290f63
Ali Alfadhul <hetler991111@gmail.com>
2025-06-04 09:30:10 +0300

    Moved routes to app-routing.module.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {ItemFormComponent} from "./components/item-form/item-form.component";
+import {CartComponent} from "./components/cart/cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  { path: 'add-item', component: ItemFormComponent },
+  { path: 'edit-item/:id', component: ItemFormComponent },
+  { path: 'cart', component: CartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import {RouterModule} from "@angular/router";
 import { ItemFormComponent } from './components/item-form/item-form.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { CartComponent } from './components/cart/cart.component';
@@ -27,12 +26,6 @@ import { CartComponent } from './components/cart/cart.component';
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
-    RouterModule.forRoot([
-      {path: '', component: DashboardComponent},
-      { path: 'add-item', component: ItemFormComponent },
-      { path: 'edit-item/:id', component: ItemFormComponent },
-      { path: 'cart', component: CartComponent }
-    ]),
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
 // and returns simulated server responses.
 // Remove it when a real server is ready to receive requests.
