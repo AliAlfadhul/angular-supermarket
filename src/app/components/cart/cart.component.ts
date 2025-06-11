ed77bf5d5a13a011d4d59328196bfe76cfadf495
Ali Alfadhul <hetler991111@gmail.com>
2025-06-11 11:54:29 +0300

    ngOnDestroy for cart component was missing.

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,26 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import {CartItem} from "../../interfaces";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
 
   cartItems: CartItem[] = [];
+
+  private destroy$ = new Subject<void>();
+
 
   constructor(
     private cartService: CartService,
     private router: Router
   ) { }
 
-  ngOnInit(): void {
-    this.cartService.cartItems$.subscribe(data => {
+  ngOnInit(): void{
+    this.cartService.cartItems$.pipe(takeUntil(this.destroy$)).subscribe(data => {
       this.cartItems = data;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   onQuantityChange(itemId: number, value: string): void {
